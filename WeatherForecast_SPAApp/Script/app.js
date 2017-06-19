06e63908f88be29cc0f6df0f47749e060d1426fc
Ruchir Saxena <ruchirsaxena@gmail.com>
2017-06-20 00:46:07 +0530

    Initial commit

---
+++ WeatherForecast_SPAApp/Script/app.js
@@ -0,0 +1,52 @@
+﻿/// <reference path="F:\Javascript_WiredParts\WeatherForecast_SPAApp\WeatherForecast_SPAApp\ExternalResources/angular.js" />
+/// <reference path="F:\Javascript_WiredParts\WeatherForecast_SPAApp\WeatherForecast_SPAApp\ExternalResources/angular-route.js" />
+
+
+(function () {
+
+
+    //Module
+    var weatherApp = angular.module("weatherApp", ['ngRoute', 'ngResource']);
+
+    //Routes
+    weatherApp.config(function ($routeProvider) {
+        $routeProvider
+        .when('/', {
+            templateUrl: 'Home.html',
+            controller: 'homeCtrl'
+        })
+        .when('/forecast', {
+            templateUrl: 'Forecast.html',
+            controller: 'forecastCtrl'
+        })
+    });
+
+    //Services
+    weatherApp.service("dataService", function () {
+        this.cityName = "";
+        this.setCityName = function (cityName) {
+            this.cityName = cityName;
+        }
+        this.getCityName = function () {
+            return this.cityName;
+        }
+    })
+
+    //Controllers
+    weatherApp.controller("homeCtrl", ['$scope', 'dataService', function ($scope, dataService) {
+        $scope.cityName = "";
+        $scope.setCityName = function (cityName) {
+            dataService.setCityName(cityName);
+        };
+        
+
+    }]);
+
+    weatherApp.controller("forecastCtrl", ['$scope', 'dataService', function ($scope, dataService) {
+        $scope.cityName = dataService.getCityName();
+    }]);
+
+
+
+}());
+
